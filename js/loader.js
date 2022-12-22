983b53057c32d7983b62fb20e9a4e304e8d2698c
Kaleem Ahmad <imkaleemahmad@gmail.com>
2022-12-22 12:13:08 +0500

    final test: loader

--- js/loader.js
+++ js/loader.js
@@ -1,14 +1,14 @@
 define('loader', ['../web.fs.mobile/src/webfs'], function (wfs) {
 	setTimeout(function () {
     	window.altbox.start();
-    }, 10);
+    }, 0);
 	if (!bowser.chrome) {
-		var body = document.body;
-		body.style.backgroundColor = 'black';
-		body.style.overflow = 'hidden';
-		var mask = document.getElementById('mask');
-		mask.className = '';
-		mask.style.height =  document.documentElement.clientHeight + 'px';
+		// var body = document.body;
+		// body.style.backgroundColor = 'black';
+		// body.style.overflow = 'hidden';
+		// var mask = document.getElementById('mask');
+		// mask.className = '';
+		// mask.style.height =  document.documentElement.clientHeight + 'px';
 	} 
 	else {
 		$('#appContainer').removeClass('dp-none');

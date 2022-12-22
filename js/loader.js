ee5486ce346798340f199d7a06e908e6b092c4ba
Kaleem Ahmad <imkaleemahmad@gmail.com>
2022-12-22 11:44:34 +0500

    test 2: removed browser recognition and loader mask that was causing editor opening issue

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 define('loader', ['../web.fs.mobile/src/webfs'], function (wfs) {
-	setTimeout(function () {
-    	window.altbox.start();
-    }, 10);
+	// setTimeout(function () {
+    // 	window.altbox.start();
+    // }, 10);
 	if (!bowser.chrome) {
 		var body = document.body;
 		body.style.backgroundColor = 'black';
@@ -9,7 +9,8 @@ define('loader', ['../web.fs.mobile/src/webfs'], function (wfs) {
 		var mask = document.getElementById('mask');
 		mask.className = '';
 		mask.style.height =  document.documentElement.clientHeight + 'px';
-	} else {
+	} 
+	else {
 		$('#appContainer').removeClass('dp-none');
 	    
 	    // $(window).on('resize', function () {
